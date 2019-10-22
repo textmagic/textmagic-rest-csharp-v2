a97ad8cbba541bedbb9ac0213cd0f8052e205ef3
Andrey Vorobyev <vorobivka@mail.ru>
2019-10-22 09:18:11 +0000

    Debug slack message

--- src/TextMagicClient/Properties/AssemblyInfo.cs
+++ src/TextMagicClient/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.675")]
-[assembly: AssemblyFileVersion("2.0.675")]
+[assembly: AssemblyVersion("2.0.676")]
+[assembly: AssemblyFileVersion("2.0.676")]
