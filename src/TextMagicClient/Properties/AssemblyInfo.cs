237118f1e0db1fbdaad0b8abb269d75bfa20c25c
Andrey Vorobyev <vorobivka@mail.ru>
2019-10-23 17:24:02 +0000

    Revert: Debug remote dockerd

--- src/TextMagicClient/Properties/AssemblyInfo.cs
+++ src/TextMagicClient/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.702")]
-[assembly: AssemblyFileVersion("2.0.702")]
+[assembly: AssemblyVersion("2.0.741")]
+[assembly: AssemblyFileVersion("2.0.741")]
